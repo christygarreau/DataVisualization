c2549f73027022d3f59936737bf70243bda253cd
christygarreau <20cgarreau@castilleja.org>
2019-05-21 21:51:48 -0700

    Step 4 - Done

--- src/DataAnalysisApp.java
+++ src/DataAnalysisApp.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.data.Table;
 
 import java.util.ArrayList;
 
@@ -20,19 +21,44 @@ public class DataAnalysisApp extends PApplet{
     }
 
     public void setup() {
+        background(255);
         textAlign(CENTER, CENTER);
         dataBank = new DataBank(this);
         dataBank.sort();
+        visualize();
     }
 
     public void draw() {
-        background(255);
-        stroke(0);
-        int sizeOfBox = 8;
-        rect(width/sizeOfBox,height/sizeOfBox,1000-1000/sizeOfBox,500-500/sizeOfBox);
-
         noLoop();
     }
+
+    private void visualize(){
+        stroke(0);
+        fill(0);
+        textAlign(CENTER);
+        final int BOX_SIZE = 10;
+        final int BOX_HEIGHT = (height-2*(height/BOX_SIZE))/9;
+        final int BOX_WIDTH = (width-2*(width/BOX_SIZE))/3;
+
+        text("YEARS",(width / BOX_SIZE) + (BOX_WIDTH/2),height / BOX_SIZE - 10);
+        text("NUMBER OF ELECTRIC CARS",width/BOX_SIZE + (BOX_WIDTH/2) + BOX_WIDTH,height / BOX_SIZE - 10);
+        text("AGGI", width/BOX_SIZE + (BOX_WIDTH/2) + 2*BOX_WIDTH, height / BOX_SIZE - 10);
+
+        for(int i = 0; i < 10; i++){//generates horizontals
+            line(width/BOX_SIZE,height/BOX_SIZE + (BOX_HEIGHT*i),width-(width/BOX_SIZE),height/BOX_SIZE + (BOX_HEIGHT*i));
+            int y = height / BOX_SIZE + BOX_HEIGHT/2 + BOX_HEIGHT*i;
+
+            if(i<9) {
+                text(dataBank.carBank.get(i).getYear(), (width / BOX_SIZE) + (BOX_WIDTH/2), y);
+                text(dataBank.carBank.get(i).getCars(), width/BOX_SIZE + (BOX_WIDTH/2) + BOX_WIDTH, y);
+                text(dataBank.carBank.get(i).getPollution(), width/BOX_SIZE + (BOX_WIDTH/2) + 2*BOX_WIDTH, y);
+            }
+        }
+        for(int i = 0; i < 4; i++){//generates verticals
+            line(width/BOX_SIZE + (BOX_WIDTH*i),height/BOX_SIZE,width/BOX_SIZE + (BOX_WIDTH*i),height-(height/BOX_SIZE));
+        }
+    }
+
     public static DataAnalysisApp getApp(){
         return app;
     }
